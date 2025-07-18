0775d105c605a28f2e8e3ae84f29cdae3a0db2d3
Olga <xotaoshi@gmail.com>
2025-07-19 00:09:24 +0300

    Add project files.

---
+++ Play/Play.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayTask1
+{/*Создайте класс Пьеса. Класс должен хранить такую информацию:
+
+Название пьесы;
+ФИО автора;
+Жанр;
+Год выпуска.
+Реализуйте в классе методы и свойства, необходимые для функционирования класса. Добавьте в классе деструктор. Напишите код для тестирования функциональности класса. Напишите код для проверки работы деструктора (например, создание и удаление объектов).*/
+    public class Play : IEnumerable<Play>, IDisposable
+    {
+        public string PlayName { get; set; }
+        public string AutorName { get; set; }
+        public string PlayGenre { get; set; }
+        public DateOnly YearOfRelease { get; set; }
+
+        public Play()
+        {
+            PlayName = string.Empty;
+            AutorName = string.Empty;
+            PlayGenre = string.Empty;
+            YearOfRelease = DateOnly.MinValue;
+
+        }
+        public Play(string playName, string autorName, string playGenre, string dateOnly)
+        {
+            PlayName = playName;
+            AutorName = autorName;
+            PlayGenre = playGenre;
+            try
+            {
+                YearOfRelease = DateOnly.Parse(dateOnly, CultureInfo.InvariantCulture);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                YearOfRelease = DateOnly.MinValue;
+            }
+        }
+        public Play(string playName, string autorName, string playGenre, DateOnly dateOnly)
+        {
+            PlayName = playName;
+            AutorName = autorName;
+            PlayGenre = playGenre;
+            YearOfRelease = dateOnly;
+
+
+        }
+
+        public IEnumerator<Play> GetEnumerator()
+        {
+           yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {PlayName}, Имя автора: {AutorName}, Жанр: {PlayGenre}, Дата выхода: {YearOfRelease}";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Выносим мусор!");
+            PlayName = string.Empty;
+            AutorName = string.Empty;
+            PlayGenre = string.Empty;
+            YearOfRelease = DateOnly.MinValue;
+            GC.SuppressFinalize(this); // Отключает вызов финализатора
+        }
+
+        ~Play()
+        {
+            Console.WriteLine("Finalizator");
+            PlayName = string.Empty;
+            AutorName = string.Empty;
+            PlayGenre = string.Empty;
+            YearOfRelease = DateOnly.MinValue;
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Play/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace PlayTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание и тестирование объектов Play
+            Play play1 = new Play("Гамлет", "Уильям Шекспир", "Трагедия", "1600-01-01");
+            Play play2 = new Play("авыаыв", "Уильям Шекспир", "Трагедия", "1600-01-01");
+  
+            Console.WriteLine(play1);
+            Console.WriteLine(play2);
+
+
+            Console.WriteLine(play2);
+            // Создание списка пьес
+            List<Play> plays = new List<Play>
+                {
+                    new Play("Вишнёвый сад", "Антон Чехов", "Драма", "1904-01-01"),
+                    new Play("На дне", "Максим Горький", "Драма", "1902-01-01")
+                };
+
+            foreach (var play in plays)
+            {
+                Console.WriteLine(play);
+                play.Dispose();
+            }
+        }
+    }
+}
